7c89d205533b330196dee4090c16f12be26b4253
Jacob <jwang@mightyoaks.com>
2023-04-05 16:59:42 -0700

    migrations

--- mighty_subscribe/mighty_payment/models.py
+++ mighty_subscribe/mighty_payment/models.py
@@ -5,18 +5,6 @@ from django.db import models
 
 class MightyPayment(models.Model):
     name = models.CharField(max_length=255)
-    email = models.EmailField()
-    phone = models.CharField(max_length=255)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    currency = models.CharField(max_length=255)
-    description = models.TextField()
-    payment_method = models.CharField(max_length=255)
-    payment_status = models.CharField(max_length=255)
-    payment_id = models.CharField(max_length=255)
-    payment_request_id = models.CharField(max_length=255)
-    payment_request_status = models.CharField(max_length=255)
-    payment_request_longurl = models.CharField(max_length=255)
-    payment_request_shorturl = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
