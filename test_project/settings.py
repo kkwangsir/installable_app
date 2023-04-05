3e034c33d3dc4e3f27c64cdf5b547f4c78d1095f
Jacob <jwang@mightyoaks.com>
2023-04-05 16:56:03 -0700

    added

--- test_project/settings.py
+++ test_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mighty_subscribe.mighty_payment',
+    'mighty_subscribe.mighty_profile'
 ]
 
 MIDDLEWARE = [
